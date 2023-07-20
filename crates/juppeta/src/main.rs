3260c712e36ded74b4ecc2ce8d176867d2ec52ed
johnnyxcy <johnny.xcy1997@outlook.com>
2023-07-20 23:41:58 +0800

    💥 feat: add juppeta dependencies

--- crates/juppeta/src/main.rs
+++ crates/juppeta/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(serde::Deserialize)]
+struct Data {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
 fn main() {
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+    let d = serde_json::from_str::<Data>(data);
+
+    match d {
+        Ok(d) => {
+            println!("name = {}", d.name);
+            println!("age = {}", d.age);
+            println!("phones = {:?}", d.phones);
+        }
+        _ => println!("error"),
+    };
+
     println!("Hello, world!");
 }
